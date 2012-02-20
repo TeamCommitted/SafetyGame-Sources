78ee6441fdf896ef94c717f5317b1999d496590b
Lorenzo Braghetto <monossido@lorenzobraghetto.com>
2012-02-20 16:01:12 +0100

    [Mobile]Primo commit App Android

---
+++ Front-end Mobile/SafetyGame/src/com/lorenzobraghetto/safetygame/View/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.lorenzobraghetto.safetygame.View;
+
+import com.lorenzobraghetto.safetygame.R;
+import com.lorenzobraghetto.safetygame.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
