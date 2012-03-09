644247e40dbaf4e4eefd38f739e711ba6fb08da6
NeoEden <neoeden@hotmail.it>
2012-03-09 19:55:33 +0100

    seconda parte delle modifiche; eliminate le cartelle backend e prova avanzamento corny, i sorgenti vanno messi in com--> safetyGame --> -end

---
+++ com/safetyGame/back/access/condivisi/Login.java
@@ -0,0 +1,43 @@
+/*
+ * Name: Login.java
+ * Package: com.safetyGame.back.condivisi
+ * Author: Alessandro Cornaglia
+ * Date:
+ * Version: 0.1
+ * Copyright: 
+ * 
+ * Changes:
+ * +----------------+---------------+-------------------------+
+ * |      Date      |   Programmer  |         Changes         |
+ * +----------------+---------------+-------------------------+
+ * |   2012/03/02   |    AlesCorn   | +Login()                |		
+ */
+ 
+ package access.condivisi;
+
+
+/**
+ * Classe contenitrice dei dati di autenticazione di un utente
+ * 
+ * @author TeamCommitted
+ * @version 0.1
+ * 
+ */
+public class Login {
+	private String username;
+	private String password;
+	private com.safetyGame.DataOra dataOra;
+	
+	/**
+	 * Costruttore della classe Login
+	 * @param u username
+	 * @param p password
+	 * @param d oggetto DataOra
+	 */
+	public Login(String u,String p,com.safetyGame.DataOra d){
+	   this.username = u;
+	   this.password = p;
+	   this.dataOra = d;
+	}
+	
+}
---
+++ com/safetyGame/desktop/logic/Timer.java
@@ -0,0 +1,35 @@
+package logic;
+
+
+/**
+ * Write a description of class Timer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Timer
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Timer
+     */
+    public Timer()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
