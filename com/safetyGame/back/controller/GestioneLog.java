35309e1c1f5bcf582faa9b59fabe4731df550cba
NeoEden <neoeden@hotmail.it>
2012-04-22 14:13:45 +0200

    [BE] modifica di GestioneLog e implementazione di UpdateLog

--- com/safetyGame/back/controller/GestioneLog.java
+++ com/safetyGame/back/controller/GestioneLog.java
@@ -4,104 +4,129 @@ import com.safetyGame.back.condivisi.*;
 
 public class GestioneLog{
    DataOra dataora;
-   UpdateLog log;
-   public GestioneLog(UpdateLog u){log=u;}
+   UpdateLog logGenerale; //o uno per tipo "generale" login / logout / domande / mod...
+   UpdateLog utente;
+   public GestioneLog(){}
    
    public void login(String username, char u){
       String s="";
-      log.login(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void chiediDomanda(String username){
       String s="";
-      log.chiediDomanda(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void posticipa(String username){
       String s="";
-      log.posticipa(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void rispondi(String username, String risposta){
       String s="";
-      log.rispondi(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }   
    public void datiD(String username){
       String s="";
-      log.datiD(s);      
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void passD(String username){
       String s="";
-      log.passD(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void mailD(String username){
       String s="";
-      log.mailD(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void getStat(String username){
       String s="";
-      log.getStat(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void passA(String username){
       String s="";
-      log.passA(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void listaD(){
       String s="";
-      log.listaD(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void addD(String username){
       String s="";
-      log.addD(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void delD(String username){
       String s="";
-      log.delD(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void nome(String username){
       String s="";
-      log.nome(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void cognome(String username){
       String s="";
-      log.cognome(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void codfis(String username){
       String s="";
-      log.codfis(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void username(String username){
       String s="";
-      log.username(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void impiego(String username){
       String s="";
-      log.impiego(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void trofei(String username){
       String s="";
-      log.trofei(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void reset(String username){
       String s="";
-      log.reset(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void badgeD(String username){
       String s="";
-      log.badgeD(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void domandeA(){
       String s="";
-      log.domandeA(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void addDomanda(){
       String s="";
-      log.addDomanda(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void remDomanda(){
       String s="";
-      log.remDomanda(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    public void logout(String username){
       String s="";
-      log.logout(s);
+      logGenerale.scrivi(s);
+      utente.scriviChiudi(s);
    }
    
 }
