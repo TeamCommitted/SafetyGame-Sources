5cf624d1a467190da573634d1cf8fa6f3b73ee09
acornagl <ale.corny@gmail.com>
2012-05-09 19:45:47 +0200

    aggiornata classe domanda (condivisi)

--- com/safetyGame/back/condivisi/Domanda.java
+++ com/safetyGame/back/condivisi/Domanda.java
@@ -6,22 +6,36 @@ package com.safetyGame.back.condivisi;
  * @author acornagl 
  * @version v0.1
  */
+import java.util.*;
+
 public class Domanda{
   private int id;
   private Punteggio punteggio;
   private String tipologia;
+  private ArrayList<String> risposte;
+  private int corretta;
+  private String testo;
+  private int rispostaData;
   
   /**
    * Costruttore con parametri della classe Domanda
    * 
-   * @param id id della domanda
-   * @param punteggio punteggio della domanda
-   * @param tipologia tipologia della domanda
+   * @param i id della domanda
+   * @param p punteggio della domanda
+   * @param ti tipologia della domanda
+   * @param r ArrayList di risposte
+   * @param c numero risposta corretta
+   * @param te testo della domanda
+   * @param rd risposta data dal dipendente alla domanda
    */
-  public Domanda(int id, Punteggio punteggio, String tipologia) {
-    this.id = id;
-    this.punteggio = punteggio;
-    this.tipologia = tipologia;
+  public Domanda(int i, Punteggio p, String ti, ArrayList<String> r, int c, String te, int rd) {
+    this.id = i;
+    this.punteggio = p;
+    this.tipologia = ti;
+    this.risposte = r;
+    this.corretta = c;
+    this.testo = te;
+    this.rispostaData = rd;
   }
   
   /**
@@ -32,9 +46,13 @@ public class Domanda{
     this.id = -1; //assegno alla nuova domanda un id illegale
     this.punteggio = null;
     this.tipologia = null;
+    this.risposte = new ArrayList<String>();
+    this.corretta = -1;
+    this.testo = null;
+    this.rispostaData = -1;
   }
 
-  /**
+/**
    * metodo che consente il recupero dell'id di una domanda
    * 
    * @return id domanda
@@ -86,6 +104,79 @@ public class Domanda{
    */
   public void setTipologia(String tipologia) {
     this.tipologia = tipologia;
+  }
+  
+  /**
+   * metodo che consente di recuperare le risposte di una domanda
+   * 
+   * @return insieme delle risposte
+   */
+  public ArrayList<String> getRisposte() {
+    return risposte;
+  }
+
+  /**
+   * metodo che consente di impostare l'insieme delle risposte di una domanda
+   * 
+   * @param risposte insieme delle risposte che si vuole assegnare ad una domanda
+   */
+  public void setRisposte(ArrayList<String> risposte) {
+    this.risposte = risposte;
+  }
+
+  /**
+   * metodo che consente di recuperare l'indice della risposta corretta
+   * 
+   * @return indice della risposta corretta
+   */
+  public int getCorretta() {
+    return corretta;
+  }
+
+  /**
+   * metodo che consente di impostare l'indice della risposta corretta
+   * 
+   * @param corretta indice della risposta corretta che si vuole assegnare
+   */
+  public void setCorretta(int corretta) {
+    this.corretta = corretta;
+  }
+
+  /**
+   * metodo che consente di recuperare il testo di una domanda
+   * 
+   * @return testo della domanda
+   */
+  public String getTesto() {
+    return testo;
+  }
+
+  /**
+   * metodo che consente di impostare il testo di una domanda
+   * 
+   * @param testo testo che si vuole assegnare alla domanda
+   */
+  public void setTesto(String testo) {
+    this.testo = testo;
+  }
+
+  /**
+   * metodo che consente di recuperare l'indice della risposta data da un dipendente alla
+   * domanda
+   * 
+   * @return indice della risposta data alla domanda
+   */
+  public int getRispostaData() {
+    return rispostaData;
+  }
+
+  /**
+   * metodo che consente di impostare l'indice della risposta data ad una domanda
+   * 
+   * @param rispostaData indice della risposta data ad una domanda
+   */
+  public void setRispostaData(int rispostaData) {
+    this.rispostaData = rispostaData;
   }
 
 }
