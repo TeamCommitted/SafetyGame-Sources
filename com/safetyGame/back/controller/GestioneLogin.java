2478f48fc16c9796ced109330d87b04084012736
Massimo Dalla Pietà <giampiero666@hotmail.it>
2012-05-20 18:53:21 +0200

    up classe GestioneLogin

--- com/safetyGame/back/controller/GestioneLogin.java
+++ com/safetyGame/back/controller/GestioneLogin.java
@@ -2,30 +2,46 @@ package com.safetyGame.back.controller;
 import com.safetyGame.back.access.*;
 import com.safetyGame.back.condivisi.*;
 
-//gestire i controlli in ogni funzione per ogni cosa
+/**
+ * Classe che si occupa di gestire i login nel sistema
+ * 
+ * @author mdallapi 
+ * @version v0.1
+ */
 
 public class GestioneLogin{
-   SqlDAOFactory accesso;
-   GestioneLog log;
-   public GestioneLogin(SqlDAOFactory s, GestioneLog l){accesso=s; log=l;}
+   private DAOLogin access;
+   private GestioneLog log;
    
-   public boolean loginLimitato(String username, String password){
-      boolean b = accesso.loginLimitato(username,password);
-      if (b)
-        log.login(username,'d');
-      return b; 
+   /**
+    * Costruttore con parametri della classe GestioneLogin
+    * 
+    * @param a riferimento alla classe che implementa l'interfaccia DAOLogin
+    * 		 l riferimento alla classe GestioneLog
+    */
+   public GestioneLogin(DAOLogin a, GestioneLog l){
+	   access = a;
+	   log = l;
    }
    
-   public boolean login(String username, String password){
-      boolean b = accesso.login(username,password);
-      char u='d'; //oppure s-->AS; a-->AA
-      if (b)
-        log.login(username,u);
-      return b; 
+   /**
+    * Metodo per il login degli utenti amministratori
+    * 
+    * @param login oggetto contenente i dati di login inseriti dall'utente
+    */
+   public boolean loginAdmin(Login login){
+      return access.loginLimitato(login);
    }
    
-   public void logout(String username){
-      accesso.logout(username);
-      log.logout(username);
+   /**
+    * Metodo per il login dei dipendenti
+    * 
+    * @param login oggetto contenente i dati di login inseriti dall'utente
+    */
+   public boolean loginUser(Login login){
+	   boolean result = access.loginLimitato(login);
+	   if(result)
+		   log.login(login);
+	   return result; 
    }
 }
