20287daa1e8a3d5f8bde379a1e261b59fb17add4
NeoEden <neoeden@hotmail.it>
2012-05-28 21:21:12 +0200

    [BE] completata classe badge. in tutto l'access rimane da implementare un metodo per i punteggi ed è finito

--- com/safetyGame/back/access/SqlDAODomande.java
+++ com/safetyGame/back/access/SqlDAODomande.java
@@ -1,13 +1,44 @@
+/*
+ * Name: SqlDAODomande.java
+ * Package: com.safetygame.back.access
+ * Author: Gabriele Facchin
+ * Date: {Data di approvazione del file}
+ * Version: 0.1
+ * Copyright: see COPYRIGHT
+ * 
+ * Changes:
+ * +----------+---------------------+---------------------
+ * |   Date   | Programmer          | Changes
+ * +----------+---------------------+---------------------
+ * | 20120528 | Gabriele Facchin    | + SqlDAODomande
+ * |          |                     | + prendiCampiDomanda
+ * |          |                     | + getDomanda
+ * |          |                     | + posticipa                
+ * |          |                     | + rispondi
+ * |          |                     | + domandeA
+ * |          |                     | + addDomanda
+ * |          |                     | + remDomanda
+ * |          |                     | + scriviSottoposta
+ * +----------+---------------------|---------------------
+ *
+ */
+
 package com.safetyGame.back.access;
 import com.safetyGame.back.condivisi.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class SqlDAODomande implements DAODomande{
   private Indirizzo serverDomande;
   private Indirizzo serverAzienda;
+   
+  public SqlDAODomande(Indirizzo azienda, Indirizzo domande){
+    serverAzienda=azienda;
+    serverDomande=domande;
+  }
   
   private Domanda prendiCampiDomanda(int id){
     ResultSet rs=serverDomande.selezione("Tipologia as t INNER JOIN Domanda as d ON d.tipologia=t.tipologia","*","ID="+id,"");
@@ -32,7 +63,7 @@ public class SqlDAODomande implements DAODomande{
       d.setAmbito(ambito);
     }
     catch(SQLException e){return null;}  
-    rs=serverDomande.selezione("Risposta INNER JOIN Domanda-Risposta ON ID=IDr","*","ID="+id,"ORDER BY IDd ASC");
+    rs=serverDomande.selezione("Risposta INNER JOIN Domanda-Risposta ON ID=IDr","*","ID="+id,"ORDER BY IDr ASC");
     Boolean trovato = false;
     ArrayList <String> risposte=new ArrayList<String>();
     String testor;
@@ -56,25 +87,54 @@ public class SqlDAODomande implements DAODomande{
     d.setRisposte(risposte);
     return d;
   }
-  
-  public SqlDAODomande(Indirizzo azienda, Indirizzo domande){
-    serverAzienda=azienda;
-    serverDomande=domande;
-  }
-    
+
   public Domanda getDomanda(Dipendente d){
-    //ritorna una domanda
-    return new Domanda();
-  }
-  public boolean posticipa(Dipendente d, Domanda dom){
-    //posticipa il timer e salva la domanda
-    return true;
-  }
-  public boolean rispondi(Dipendente d, Domanda dom){
-    //imposta la risposta data
-    return true;
+    ResultSet rs=serverAzienda.selezione("Storico","ID","IDdipendente="+d.getId()+" AND punteggio=-1","");
+    int id=-1;
+    boolean domanda=true;
+    try{
+      id=rs.getInt("ID");
+    }
+    catch(SQLException e){domanda=false;}  
+    if (!domanda){//non c'è una domanda non risposta, devo prelevare una nuova domanda, mi costruisco un insieme di domande corrette per l'utente (errate + non risposte)
+      rs=serverAzienda.selezione("Storico as s RIGHT JOIN Domanda as d ON s.IDdomanda=d.ID","d.ID","IDdipendente="+d.getId()+" AND (punteggio=0 OR punteggio=null)","");
+      ArrayList<Integer> idQuery = new ArrayList<Integer>();
+      boolean trovato=false;
+      while(!trovato){
+        try{
+          id=rs.getInt("ID");
+          idQuery.add(id);
+        }
+        catch(SQLException e){trovato=true;}
+      }
+      Random r=new Random();
+      id=r.nextInt(idQuery.size());
+      id=idQuery.get(id);
+    }
+    //c'è una domanda all'interno dello storico a cui l'utente non ha ancora risposto (id non modificato) oppure ne ho prelevata una (id modificato)
+    return prendiCampiDomanda(id);
   }
   
+  public boolean posticipa(Dipendente d, Domanda dom){
+    return serverAzienda.modificaRiga("Storico","punteggio=-1","IDdipendente="+d.getId()+" AND IDdomanda="+dom.getId());
+  }
+  
+  public boolean rispondi(Dipendente d, Domanda dom){
+    int punti;
+    if(dom.getRispostaData()==-1){
+      punti=0;
+    }
+    else{
+      if(dom.getRispostaData()==dom.getCorretta()){
+        punti=dom.getPunteggio().getPunti();
+      }
+      else{
+        punti=(dom.getPunteggio().getPunti())/2;
+      }
+    }
+    return serverAzienda.modificaRiga("Storico","punteggio="+punti,"IDdipendente="+d.getId()+" AND IDdomanda="+dom.getId());
+  }
+
   public ArrayList<Domanda> domandeA(){
     ResultSet rs=serverAzienda.selezione("Domanda","ID","","ORDER BY ID ASC");
     ArrayList<Integer> id = new ArrayList<Integer>();
@@ -109,17 +169,18 @@ public class SqlDAODomande implements DAODomande{
     }
     return domande;
   }
-  
+
   public boolean addDomanda(Domanda d){
-    //aggiunge una domanda
-    return true;
+    String s[]=new String[0];
+    s[0]=""+d.getId();
+    return serverAzienda.inserisciRiga("Domanda","ID",s);
   }
+  
   public boolean remDomanda(Domanda d){
-    //rimuove una domanda
-    return true;
+    return serverAzienda.cancellaRiga("Domanda","ID="+d.getId());
   }
+  
   public boolean scriviSottoposta(Dipendente dip, Domanda dom){
-    //che scrive sul DB che la domanda dom Ã¨ stata sottoposta al Dipendente dip  
-    return true;
+    return serverAzienda.modificaRiga("Storico","punteggio=0","IDdipendente="+dip.getId()+" AND IDdomanda="+dom.getId());
   }
 }
