13e73ece0db71855a7ad41b2ffc2a7cfd4fee90a
acornagl <ale.corny@gmail.com>
2012-06-11 18:17:12 +0200

    ripristinato Inizializzatore.java cancellato erroneamente

---
+++ com/safetyGame/back/Inizializzatore.java
@@ -0,0 +1,11 @@
+package com.safetyGame.back;
+
+ 
+
+public class Inizializzatore{
+   private Inizializzatore(){}
+   public void main (String[] args){
+      // inizializzatore degli oggetti da passare ad ogni oggetto partendo dal basso
+      System.out.println("ciao");
+   }
+}
