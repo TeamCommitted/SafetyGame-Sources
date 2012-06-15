650789dcec7e297835ce4c327d2d5321a753ef67
acornagl <ale.corny@gmail.com>
2012-06-15 19:02:37 +0200

    popolazione az corretta

--- SQL/popAz.sql
+++ SQL/popAz.sql
@@ -1,6 +1,5 @@
 insert into Ruolo values ('Pompiere'),('Magazziniere'),('Dirigente'),('Segretaria');
-insert into Dipendente(nome,cognome,codice_fiscale,email,nickname,password,ruolo)values ('Alberto','Boi','sxrYDTCfvg','teamcommitted@gmail.com','nick','pass','Pompiere');
-insert into Assegnato values (1,1,20120614);
+insert into Dipendente(ID,nome,cognome,codice_fiscale,email,nickname,password,ruolo)values (1,'Alberto','Boi','sxrYDTCfvg','teamcommitted@gmail.com','nick','pass','Pompiere');
 insert into Amministratore values (1,'teamcommitted@gmail.com','amministratoreAz','pass','','2012/06/11 12:23:32','esrdftgy',true);
-insert into Badge values (1,'Re dei pompieri', 'tanti punti su incendio',100),(2,'Re dei segretari', 'tanti punti su interno',100)
-insert into Assegnato values (1,2,'2012/06/14 12:13:45');
+insert into Badge values (1,'Re dei pompieri', 'tanti punti su incendio',100),(2,'Re dei segretari', 'tanti punti su interno',100);
+insert into Assegnato values (1,1,'2012/06/14 12:13:45'),(1,2,'2012/06/14 12:13:45');
