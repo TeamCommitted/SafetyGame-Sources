cfd8605782aa2b4869e7c9f3781412f10c78b00b
acornagl <ale.corny@gmail.com>
2012-06-16 06:34:57 +0200

    primo avanzamento test timer(non va)

--- com/safetyGame/desktop/logic/TimerTest.java
+++ com/safetyGame/desktop/logic/TimerTest.java
@@ -6,9 +6,19 @@ import org.junit.Test;
 
 public class TimerTest {
 
+	private Timer timer;
+	private void init() {
+	  timer =  new Timer(6000);
+	}
 	@Test
 	public void runTest() {
-		fail("Not yet implemented");
+	//test del run del timer
+	  init();
+	  timer.start();
+	 /* while(!timer.getFinito()){
+	  try{timer.start();}catch(Exception e){System.out.println("www");System.out.println(e.getMessage());}
+	  
+	  System.out.println(timer.getFinito());}*/
 	}
 
 }
