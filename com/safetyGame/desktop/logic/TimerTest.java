0a574a7477591765d94b1797926a2e452021c81e
acornagl <ale.corny@gmail.com>
2012-06-16 05:45:46 +0200

    vai cetto alla riscossa

---
+++ com/safetyGame/desktop/logic/TimerTest.java
@@ -0,0 +1,14 @@
+package com.safetyGame.desktop.logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TimerTest {
+
+	@Test
+	public void runTest() {
+		fail("Not yet implemented");
+	}
+
+}
