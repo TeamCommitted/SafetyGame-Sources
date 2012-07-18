38ffd887af43d785d95029e863f7f5c37e44d304
acornagl <ale.corny@gmail.com>
2012-07-18 12:16:57 +0200

    assegnati nomi significativi alle varibili di ApplicationConnection

--- com/safetyGame/back/connection/ApplicazioniConnection.java
+++ com/safetyGame/back/connection/ApplicazioniConnection.java
@@ -10,6 +10,11 @@
  * +----------+---------------------+---------------------
  * |   Date   | Programmer          | Changes
  * +----------+---------------------+---------------------
+ * | 20120718 |Alessandro Cornaglia | Tutti i metodi sono
+ * |          |                     | stati aggiornati assegnado
+ * |          |                     | a alle variabili 
+ * |          |                     | nomi significativi
+ * +----------+---------------------+---------------------
  * | 20120611 | Lorenzo Braghetto   | ApplicazioniConnection
  * |          |                     | login
  * |          |                     | getDati
@@ -43,40 +48,41 @@ public class ApplicazioniConnection{
   /**
    * Costruttore della classe WebConnection
    * 
-   * @param d riferimento al facade GestioneDati 
+   * @param gestDati riferimento al facade GestioneDati 
    */
-  public ApplicazioniConnection(GestioneDati d){dati = d;}
+  public ApplicazioniConnection(GestioneDati gestDati){dati = gestDati;}
     
   /**
    * Metodo per il login dei dipendenti
    * 
-   * @param login oggetto contenente i dati di login inseriti dall'utente
+   * @param username username dell'utente
+   * @param password password dell'utente
    * @return boolean che conferma il successo o meno dell'operazione
    */
   public boolean login(String username, String password){
-    Login l=new Login(username,password);
-    return dati.loginUser(l);
+    Login login=new Login(username,password);
+    return dati.loginUser(login);
   }
     
   /**
    * Metodo che consente di reperire le informazioni di un dipendente a partire
    * dal suo login
    * 
-   * @param l login del dipendente
+   * @param login login del dipendente
    * @return informazioni sul dipendente
    */
-  public Dipendente getDati(Login l){
-    return dati.getDatiD(l);
+  public Dipendente getDati(Login login){
+    return dati.getDatiD(login);
   }
    
    /**
     * Metodo che consente di recuperare le statistiche globali
     * 
-    * @param l login del dipendente che richiede di conoscere le statistiche
+    * @param login login del dipendente che richiede di conoscere le statistiche
     * @return statistiche globali
     */ 
-  public Punteggio getStat(Login l){
-    return dati.getStatisticheGlob(l);
+  public Punteggio getStat(Login login){
+    return dati.getStatisticheGlob(login);
   }
    
   /**
@@ -85,80 +91,80 @@ public class ApplicazioniConnection{
    * @param dip dipendente che chiede di modificare la password 
    * @return true se operazione riuscita con successo, false altrimenti
    */
-  public boolean modPass(Dipendente d){
-    return dati.modificaPass(d);
+  public boolean modPass(Dipendente dip){
+    return dati.modificaPass(dip);
   }
    
   /**
    * Metodo che consente la modifica della mail da parte di un dipendente
    * 
-   * @param d Dipendente che chiede di modificare la mail
+   * @param dip Dipendente che chiede di modificare la mail
    * @param mail nuovo indirizzo mail da inserire
    * @return true se operazione riuscita con successo, false altrimenti
    */
-  public boolean modMail(Dipendente d, String mail){
-    return dati.modificaEmail(d,mail);
+  public boolean modMail(Dipendente dip, String mail){
+    return dati.modificaEmail(dip,mail);
   }
    
   /**
    * Metodo che consente la rigenerazione della password per un dipendente
    * 
-   * @param d Dipendente che chiede di modificare la mail
+   * @param recupero oggetto recupero contentente le informazioni del dipendente utili al recupero password
    * @return true se operazione riuscita con successo, false altrimenti
    */
-  public boolean resetPass(Recupero r){
-    return dati.recuperoD(r);
+  public boolean resetPass(Recupero recupero){
+    return dati.recuperoD(recupero);
   }
    
   /**
    * Metodo che consente di recuperare una domanda 
    * 
-   * @param l oggetto Login del dipendente che deve ricevere la domanda
+   * @param login oggetto Login del dipendente che deve ricevere la domanda
    * @return domanda per il login proposto
    */
-  public Domanda mostraDomanda(Login l){
-    return dati.getDomandaD(l);
+  public Domanda mostraDomanda(Login login){
+    return dati.getDomandaD(login);
   }
   
    /**
     * Metodo per posticipare una domanda
     * 
-    * @param l login del dipendente
-    * @param d domanda posticipata
+    * @param login login del dipendente
+    * @param dom domanda posticipata
     * @return true se la domanda è stata posticipata correttamente, false altrimenti
     */
-  public boolean posticipa(Login l, Domanda d){
-    return dati.posticipa(l, d);
+  public boolean posticipa(Login login, Domanda dom){
+    return dati.posticipa(login, dom);
   }
    
   /**
    * Metodo che si occupa di comunicare la risposta del Dipendente
    * 
-   * @param l Login del dipendente che ha risposto
-   * @param risposta Domanda posta al dipendente contenente la risposta data
+   * @param login Login del dipendente che ha risposto
+   * @param domanda Domanda posta al dipendente contenente la risposta data
    * @return boolean che indica se l'operazione e` andata o meno a buon fine
    */
-  public boolean rispondi(Login l, Domanda d){
-    return dati.setRisposta(l, d);
+  public boolean rispondi(Login login, Domanda domanda){
+    return dati.setRisposta(login, domanda);
   }
   
   /**
    * Metodo per segnalare al sistema il logout di un utente
    * 
-   * @param l login del dipendente che ha effettuato il logout
+   * @param login login del dipendente che ha effettuato il logout
    */
-  public void logout(Login l){
-    dati.logout(l);
+  public void logout(Login login){
+    dati.logout(login);
   }
 
   /**
     * Metodo per ottenere i dati dei badge per un dato utente
     * 
     * @param login dati dell'utente che effettua la richiesta
-    * @param n numero di badge che si vuole selezionare
+    * @param num numero di badge che si vuole selezionare
     * @return un ArrayList<Badge> contenente n badge ottenuti dall'utente 
   */
-  public ArrayList<Badge> getBadge(Login l, int n){
-    return dati.getBadgeD(l,n);
+  public ArrayList<Badge> getBadge(Login login, int num){
+    return dati.getBadgeD(login,num);
   }
 }
